5a60aac318153efdc2ed59fc6f41b25e9e6c99a6
Ash Mishra <a.mishra@nfb.ca>
2019-12-02 09:30:02 -0800

    try catch block around possible keyvalue deallocated when exiting fullscreen

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -1494,8 +1494,12 @@ static int const RCTVideoUnset = -1;
   
   [self removePlayerLayer];
   
-  [_playerViewController.contentOverlayView removeObserver:self forKeyPath:@"frame"];
-  [_playerViewController removeObserver:self forKeyPath:readyForDisplayKeyPath];
+  try {
+    [_playerViewController.contentOverlayView removeObserver:self forKeyPath:@"frame"];
+    [_playerViewController removeObserver:self forKeyPath:readyForDisplayKeyPath];
+  }@catch(id anException){
+  }
+
   [_playerViewController.view removeFromSuperview];
   _playerViewController.rctDelegate = nil;
   _playerViewController.player = nil;
